6dfc64aab5e5fa891ec1eed16dac75c90316a686
Satnam-Gill <ersatnamgill@gmail.com>
2025-04-15 11:20:37 +0530

    (Update: Metas)

--- app/areas-we-serve/[State]/page.tsx
+++ app/areas-we-serve/[State]/page.tsx
@@ -17,8 +17,6 @@ import CounterCta from "@/app/components/Widgets/CounterCta";
 import HourCta from "@/app/components/Home/HourCta";
 import Guarantees from "@/app/components/Widgets/Guarantees";
 import ReviewWidget from "@/app/components/Widgets/ReviewWidget";
-import data from "@/components/Content/serviceWidgetContent.json";
-import Types from "@/app/components/Widgets/Types";
 import Affordable from "@/app/components/Home/Affordable";
 import ProcessWidget from "@/app/components/Widgets/ProcessWidget";
 import AreaWeServe from "@/app/components/Widgets/AreaWeServe";
@@ -35,7 +33,7 @@ export function generateMetadata({ params }: SubdomainPageProps) {
   const ContentData = cityData[State];
   return {
     title: ContentData?.metaTitle,
-    description: ContentData?.metaDescription,
+    description: `${ContentData?.metaDescription.replace("Call now!", `Call us at ${ContactInfo.No}`)}.`,
     alternates: {
       canonical: `${ContactInfo.baseUrl}areas-we-serve/${State}/`,
     },
@@ -78,8 +76,7 @@ export default function SubdomainPage({ params }: SubdomainPageProps) {
     "@context": "https://schema.org",
     "@type": "LocalBusiness",
     name: `${ContactInfo.name}`,
-    image:
-      `${ContactInfo.logo}` || "",
+    image: `${ContactInfo.logo}` || "",
     "@id": `${ContactInfo.baseUrl}`,
     url: `${ContactInfo.baseUrl}`,
     telephone: `${ContactInfo.No}`,
@@ -141,7 +138,7 @@ export default function SubdomainPage({ params }: SubdomainPageProps) {
   };
   return (
     <div className="mx-auto max-w-[2100px] overflow-hidden">
-    <section>
+      <section>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
@@ -151,53 +148,55 @@ export default function SubdomainPage({ params }: SubdomainPageProps) {
         h1={`${ContentData.h1Banner} ${ContentData.zipCodes && ContentData.zipCodes.split("|")[0]}`}
         image={ContentData.bannerImage}
         header={ContentData.bannerQuote}
-        p1={ContentData.metaDescription}
+        p1={`${ContentData?.metaDescription.replace("Call now!", `Call us at ${ContactInfo.No}`)}.`}
       />
       {/* Section 1 */}
       {/* <p>{subDomain.map((item:any)=>(
         <p>{item}</p>
       ))}</p> */}
-      <div className="mt-14 grid w-full grid-cols-1 gap-6  px-6 md:mt-28 md:grid-cols-2 md:px-24 items-center">
+      <div className="mt-14 grid w-full grid-cols-1 items-center  gap-6 px-6 md:mt-28 md:grid-cols-2 md:px-24">
         <div className=" h-full">
           <Image
             height={1000}
             width={1000}
             src={`${ContentData?.h2Image}`}
             className="h-full w-full  rounded-lg object-cover shadow-lg"
-            alt={ContentData?.h2Image.split("/").pop()?.split(".")[0] || "image"}
+            alt={
+              ContentData?.h2Image.split("/").pop()?.split(".")[0] || "image"
+            }
           />
         </div>
-          <div className=" flex w-full flex-col gap-3 ">
-            <span className="text-sm font-bold text-main">
-              {ContentData?.name} {ContactInfo.name}{" "}
-              Services
-            </span>
-            <h2 className="text-3xl font-bold">{ContentData?.h2}</h2>
+        <div className=" flex w-full flex-col gap-3 ">
+          <span className="text-sm font-bold text-main">
+            {ContentData?.name} {ContactInfo.name} Services
+          </span>
+          <h2 className="text-3xl font-bold">{ContentData?.h2}</h2>
 
-            <div
-              className="mt-3  text-justify"
-              dangerouslySetInnerHTML={{ __html: ContentData?.p2 }}
-            ></div>
-            <div className="gap-4 md:flex">
-              <div className="rounded-lg bg-gray-100 p-4 shadow-lg">
-                <h4 className="text-xl font-bold">
-                  Residential {ContactInfo.name} Services
-                </h4>
-                <p>
-                  Professional Residential {ContactInfo.service} in{" "}
-                  {ContentData?.name}, {State.split("-").pop()?.toUpperCase()}.
-                </p>
-              </div>
-              <div className="rounded-lg bg-gray-100 p-4 shadow-lg">
-                <h4 className="text-xl font-bold">
-                  Commercial {ContactInfo.name} Services
-                </h4>
-                <p>
-                  Commercial {ContactInfo.service} in {ContentData?.name}, {State.split("-").pop()?.toUpperCase()}.
-                </p>
-              </div>
+          <div
+            className="mt-3  text-justify"
+            dangerouslySetInnerHTML={{ __html: ContentData?.p2 }}
+          ></div>
+          <div className="gap-4 md:flex">
+            <div className="rounded-lg bg-gray-100 p-4 shadow-lg">
+              <h4 className="text-xl font-bold">
+                Residential {ContactInfo.name} Services
+              </h4>
+              <p>
+                Professional Residential {ContactInfo.service} in{" "}
+                {ContentData?.name}, {State.split("-").pop()?.toUpperCase()}.
+              </p>
+            </div>
+            <div className="rounded-lg bg-gray-100 p-4 shadow-lg">
+              <h4 className="text-xl font-bold">
+                Commercial {ContactInfo.name} Services
+              </h4>
+              <p>
+                Commercial {ContactInfo.service} in {ContentData?.name},{" "}
+                {State.split("-").pop()?.toUpperCase()}.
+              </p>
             </div>
           </div>
+        </div>
       </div>
       {/* Section 1 */}
       {/* Section 2 */}
@@ -425,11 +424,13 @@ export default function SubdomainPage({ params }: SubdomainPageProps) {
       {/* Area we Serve */}
       {slugs.length > 0 && (
         <div id="area-we-serve" className="pt-14 md:pt-28">
-        <h2 className={`  text-center text-3xl font-bold text-main`}>Cities We Serve </h2>
-        <AreaWeServe slugs={slugs} />
-      </div>
-    )}
-      
+          <h2 className={`  text-center text-3xl font-bold text-main`}>
+            Cities We Serve{" "}
+          </h2>
+          <AreaWeServe slugs={slugs} />
+        </div>
+      )}
+
       {/* Area we Serve */}
       {/* Neighborhood */}
       {/* {ContentData?.neighbourhoods ? (
