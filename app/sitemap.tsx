36babe7968a4bf67a71b672d87cb95be9fab355b
gundeeep <gundeeep@gmail.com>
2025-10-15 13:16:28 +0530

    Add: Neighbourhood Page

--- app/sitemap.tsx
+++ app/sitemap.tsx
@@ -8,7 +8,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   const BaseUrl = contentData.baseUrl;
   const CityData: any = CityData1;
   const SubDomain: any = Object.keys(data);
-  const ServiceSlug: any = serviceData.lists.map((item:any) => item.slug);
+  const ServiceSlug: any = serviceData.lists.map((item: any) => item.slug);
   const SubDomainURL = SubDomain.map((location: any) => ({
     url: `${contentData.baseUrl}areas-we-serve/${location}`,
     lastModified: new Date(),
@@ -21,6 +21,40 @@ export default function sitemap(): MetadataRoute.Sitemap {
     changeFrequency: "weekly",
     priority: 0.7,
   }));
+
+  // Generate neighborhood URLs
+  const NeighborhoodURLs: any[] = [];
+ Object.values(data).map((city: any) => {
+  if (city.neighbourhoods) {
+    const neighborhoods = city.neighbourhoods.split("|");
+    neighborhoods.forEach((neighborhood: string) => {
+      const formattedNeighborhood = neighborhood
+        .trim()
+        .toLowerCase()
+        .replace(/\.+$/, "")
+        .replace(/\s+/g, "-");
+
+      const url = `${contentData.baseUrl}areas-we-serve/${city.slug}/${formattedNeighborhood}/`;
+
+      // Debug for invalid URLs
+      if (/[&<>]/.test(url)) {
+        console.log("⚠️ Invalid character in URL:", url);
+      }
+      if (url.includes("undefined")) {
+        console.log("⚠️ Undefined value in URL:", url);
+      }
+
+      NeighborhoodURLs.push({
+        url: encodeURI(url),
+        lastModified: new Date(),
+        changeFrequency: "weekly",
+        priority: 0.9,
+      });
+    });
+  }
+});
+
+
   // console.log(ServiceSlug);
 
   return [
@@ -43,6 +77,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
+      url: `${contentData.baseUrl}our-brands`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
       url: `${contentData.baseUrl}services`,
       lastModified: new Date(),
       changeFrequency: "monthly",
@@ -56,5 +96,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     ...SubDomainURL,
+    ...NeighborhoodURLs,
   ];
 }
